9dfbec0d415659bb7539adb73b34b2114795f147
janulimoelv <49278348+janulimoelv@users.noreply.github.com>
2019-04-04 22:55:17 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## function makeCacheMatrix creates a list of functions which "contain" the data from the original-matrix by 
+## using the data from the "frame-environment" because of lexical scoping ...
 
+
+
+makeCacheMatrix <- function(x = matrix()) {
+        
+        m <- NULL
+        set <- function(y) { 
+                x <<- y   
+                m <<- NULL }
+        get <- function() x
+        settestmatrix <- function(matrice) m <<- matrice
+        gettestmatrix <- function() m
+        
+        list(set = set, get = get,
+             settestmatrix = settestmatrix,
+             gettestmatrix = gettestmatrix) 
+           
 }
 
 
 ## Write a short comment describing this function
+## function calculates the inverse of a matrix but if it was calculated before it returns value direct fdrom "cache" instead of recalculating
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+        m <- x$gettestmatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$settestmatrix(m)
+        m
 }
